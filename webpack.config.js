d9e3f4016de3005c4eda323637e5289799664cac
Viacheslav-ua <75425740+Viacheslav-ua@users.noreply.github.com>
2023-11-18 06:00:24 +0200

    plagins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = (env) => {
   return {
@@ -11,5 +13,10 @@ module.exports = (env) => {
     filename: '[name].[contenthash].js',
     clean: true,
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
+    new webpack.ProgressPlugin(),
+  ],
 };
 }
