5eabd1228d5b54cbbb665d13159713a5f22b110c
Viacheslav-ua <75425740+Viacheslav-ua@users.noreply.github.com>
2023-11-19 04:20:26 +0200

    MiniCssExtractPlugin

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 type Mode = 'production' | 'development'
 interface EnvVariables {
@@ -12,6 +13,7 @@ interface EnvVariables {
 export default (env: EnvVariables) => {
 
   const isDev = env.mode === 'development'
+  const isProd = env.mode === 'production'
 
   const config: webpack.Configuration = {
     mode: env.mode ?? "development",
@@ -27,6 +29,10 @@ export default (env: EnvVariables) => {
   plugins: [
     new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
     isDev && new webpack.ProgressPlugin(),
+    isProd && new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css',
+    })
   ].filter(Boolean),
 
   module: {
@@ -35,7 +41,7 @@ export default (env: EnvVariables) => {
         test: /\.s[ac]ss$/i,
         use: [
           // Creates `style` nodes from JS strings
-          "style-loader",
+          isDev ? "style-loader" : MiniCssExtractPlugin.loader,
           // Translates CSS into CommonJS
           "css-loader",
           // Compiles Sass to CSS
